7b406c94bf3ba78916c9710c387117d5749db8e6
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-08-18 13:24:48 +0300

    Add operate function and limitDecimalNumbers

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let operator;
+let num1;
+let num2;
+
+
 function add(num1 , num2){
     return num1 + num2;
 }
@@ -12,4 +17,25 @@ function divide(num1 , num2){
         return "ERROR"
     }
     return num1 / num2;
+}
+
+function operate(num1 , operator , num2){
+    let operation = add
+    
+    if (operator == "-"){
+        operation = subtract
+
+    }else if (operator == "/"){
+        operation = divide
+
+    }else if (operator == "*"){
+        operation = multiply
+    }
+
+    return limitDecimalNumbers(operation(num1 , num2))
+}
+// helper function
+function limitDecimalNumbers(number,decimalLimit=3){
+    let limit = 10 ** decimalLimit
+    return Math.floor(number * limit) / limit
 }
