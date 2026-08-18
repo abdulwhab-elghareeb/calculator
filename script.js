3e8687eee77eafba3493856e1bf80f6544bd4cd2
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-08-18 12:53:48 +0300

    Add basic math functions
    functions for :
    add
    subtract
    multiply
    divide

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(num1 , num2){
+    return num1 + num2;
+}
+function subtract(num1 , num2){
+    return num1 - num2;
+}
+function multiply(num1 , num2){
+    return num1 * num2;
+}
+function divide(num1 , num2){
+    if (num1 === 0 || num2 === 0){
+        return "ERROR"
+    }
+    return num1 / num2;
+}
