c71bc2e4473e25a1d5e4ca6b1c087b2a8cdc9a0d
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-08-18 20:15:35 +0300

    Add functionality to the calculator

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let operator;
-let num1;
-let num2;
+let operator = {
+    type: "",
+    isClicked: false,
+}
+
+let num1 = ""
+let num2 = "";
 
 
 function add(num1 , num2){
@@ -13,9 +17,6 @@ function multiply(num1 , num2){
     return num1 * num2;
 }
 function divide(num1 , num2){
-    if (num1 === 0 || num2 === 0){
-        return "ERROR"
-    }
     return num1 / num2;
 }
 
@@ -26,16 +27,78 @@ function operate(num1 , operator , num2){
         operation = subtract
 
     }else if (operator == "/"){
+        if (num1 == 0|| num2 == 0){
+            return "Error"
+        }
         operation = divide
 
-    }else if (operator == "*"){
+    }else if (operator == "*" || operator == "x"){
         operation = multiply
     }
 
     return limitDecimalNumbers(operation(num1 , num2))
 }
+displayScreenText = document.querySelector(".text")
+clearButton = document.querySelector(".clear-btn")
+numbers = document.querySelectorAll(".number-btn")
+operators = document.querySelectorAll(".operator-btn")
+equalSign = document.querySelector(".equal-btn")
+
+clearButton.addEventListener("click" , (event) => {
+    clear()
+})
+
+numbers.forEach((numberButton) =>{
+    numberButton.addEventListener("click" , (event) =>{
+        targetContent = event.target.textContent
+        if(!(operator.isClicked)){
+            num1 += targetContent
+            console.log(num1)
+            displayScreenText.textContent = num1
+        }else{
+            num2 += targetContent
+            displayScreenText.textContent = num2    
+        }
+        
+    })
+})
+
+operators.forEach((operatorButton) => {
+    operatorButton.addEventListener("click" , (event) =>{
+            operator.type = event.target.textContent;
+            operator.isClicked = true
+        if (num2){
+            let clickEvent = new Event("click")
+            equalSign.dispatchEvent(clickEvent)
+            prevResult = displayScreenText.textContent
+            num1 = prevResult
+            
+        }
+    })
+    })
+equalSign.addEventListener("click", (event) =>{
+    if(num1 && num2 && operator.type){
+        result = operate(Number(num1) , operator.type , Number(num2))
+        clear()
+
+        displayScreenText.textContent = result
+    }
+})
+
+
+
+
+
+
 // helper function
 function limitDecimalNumbers(number,decimalLimit=3){
     let limit = 10 ** decimalLimit
     return Math.floor(number * limit) / limit
+}
+function clear(){
+    num1  = ""
+    num2  = ""
+    displayScreenText.textContent = ""
+    operator.isClicked = false
+    operator.type = ""
 }
